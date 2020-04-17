b4aca8993c793921f296e8d1de993db6d5e01a31
ivan Jankovic <jankoemail@gmail.com>
2020-04-17 14:18:42 -0400

    Added Auth0 values

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -9,18 +9,22 @@ AUTH0_DOMAIN = 'jankovic.auth0.com'
 ALGORITHMS = ['RS256']
 API_AUDIENCE = 'coffee'
 
-## AuthError Exception
+# ----------------------------------------
+#           AuthError Exception
+# ----------------------------------------
 '''
 AuthError Exception
 A standardized way to communicate auth failure modes
 '''
+
+
 class AuthError(Exception):
     def __init__(self, error, status_code):
         self.error = error
         self.status_code = status_code
 
 
-## Auth Header
+# Auth Header
 
 
 # DONE implement get_token_auth_header() method
@@ -64,14 +68,15 @@ def get_token_auth_header():
 
 
 # DONE implement check_permissions(permission, payload) method
-#     - INPUTS
-#         - permission: string permission (i.e. 'post:drink')
-#         - payload: decoded jwt payload
+#   - INPUTS
+#     - permission: string permission (i.e. 'post:drink')
+#     - payload: decoded jwt payload
 
-#     - it should raise an AuthError if permissions are not included in the payload
-#         !!NOTE check your RBAC settings in Auth0
-#     - it should raise an AuthError if the requested permission string is not in the payload permissions array
-#     - return true otherwise
+#   - it should raise an AuthError if permissions are not included in payload
+#     !!NOTE check your RBAC settings in Auth0
+#   - it should raise an AuthError if the requested permission string
+#     is not in the payload permissions array
+#   - return true otherwise
 
 def check_permissions(permission, payload):
     print(permission)
@@ -99,7 +104,8 @@ def check_permissions(permission, payload):
 #     - it should validate the claims
 #     - return the decoded payload
 
-#     !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
+# !!NOTE urlopen has a common certificate error described here:
+#       https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
 
 def verify_decode_jwt(token):
     jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
@@ -142,7 +148,8 @@ def verify_decode_jwt(token):
         except jwt.JWTClaimsError:
             raise AuthError({
                 'code': 'invalid_claims',
-                'description': 'Incorrect claims. Please, check the audience and issuer.'
+                'description':
+                    'Incorrect claims. Please, check the audience and issuer.'
             }, 401)
         except Exception:
             raise AuthError({
@@ -161,8 +168,10 @@ def verify_decode_jwt(token):
 
 #     - it should use the get_token_auth_header method to get the token
 #     - it should use the verify_decode_jwt method to decode the jwt
-#     - it should use the check_permissions method validate claims and check the requested permission
-#     - return the decorator which passes the decoded payload to the decorated method
+#     - it should use the check_permissions method validate claims
+#       and check the requested permission
+#     - return the decorator which passes the decoded payload
+#       to the decorated method
 
 def requires_auth(permission=''):
     def requires_auth_decorator(f):
@@ -171,7 +180,7 @@ def requires_auth(permission=''):
             token = get_token_auth_header()
             try:
                 payload = verify_decode_jwt(token)
-            except:
+            except Exception:
                 raise AuthError({
                     'code': 'invalid_token',
                     'description': 'Token could not be verified.'
